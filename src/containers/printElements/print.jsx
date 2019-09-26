2e2079b82eb96ca1f01c9a14b1bfdea49b469252
bjmrq <bmarquiscom@gmail.com>
2019-09-26 07:07:44 -0400

    test print 1 notprint hoc

--- src/containers/printElements/print.jsx
+++ src/containers/printElements/print.jsx
@@ -25,7 +25,7 @@ class Print extends PureComponent {
       setTimeout(() => {
         this.props.printingToogle(false);
         this.setState({ open: true });
-      }, 2000);
+      }, 1000);
     }, 10);
   }
 
