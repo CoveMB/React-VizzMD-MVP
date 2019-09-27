a58793010c3c08a3b24a6993cc3a86fe44ef3f70
Benjamin Marquis <bmarquiscom@gmail.com>
2019-09-27 16:06:32 -0400

    test print

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 // external modules
+import TagManager from 'react-gtm-module';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -8,6 +9,15 @@ import reducers from './reducers';
 // internal modules
 import App from './components/App';
 import '../assets/stylesheets/application.scss';
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  const tagManagerArgs = {
+    gtmId: 'GTM-MM93V8P'
+  };
+
+  TagManager.initialize(tagManagerArgs);
+}
+
 
 const root = document.getElementById('root');
 // render an instance of the component in the DOM
